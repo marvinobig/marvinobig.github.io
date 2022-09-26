6534e8cb9f005aa09f7b062efce5f757462e4271
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-09-26 10:04:34 +0100

    update about me

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -7,26 +7,23 @@ function AboutMe() {
     <header className={about.about} id="about">
       <h2 className={about.title}>Hi, I'm Marvin</h2>
       <p>
-        I'm a <span className={about.important}>fullstack developer</span>. The
-        summer was about to end and I was heading into my third year at
-        university. I was just googling around and I came across freeCodeCamp.
-        That was when my journey into development really began. It was through
-        freeCodeCamp that I learnt{" "}
-        <span className={about.important}>HTML & CSS</span>.
+        I'm a <span className={about.important}>fullstack developer</span>.
       </p>
 
       <p>
-        University taught me <span className={about.important}>JavaScript</span>{" "}
-        but The Odin Project taught me to understand it, taught me to enjoy it
-        and then taught me how to really learn. After my first experience with
-        coding, I realised that this is something{" "}
-        <span className={about.important}>I enjoy more than anything</span>,
-        being able to have a mixture of{" "}
-        <span className={about.important}>creativity and logic</span> and teach
-        myself how to turn an idea into a product/service or develop someone's
-        ideas into reality.{" "}
-        <span className={about.important}>Download my CV/resume</span> with the
-        button below.
+        Having always valued{" "}
+        <span className={about.important}>
+          continuous learning and self-improvement
+        </span>
+        , I enrolled at Northcoders to learn and develop my knowledge within the
+        Software Development field.{" "}
+        <span className={about.important}>Northcoders</span> has allowed me to
+        be surrounded by peers of likeminded junior developers, which I find
+        invaluable for motivation and success. Prior to Northcoders, I had been
+        learning to code on my own for around one year, using resources such as{" "}
+        <span className={about.important}>
+          freeCodeCamp, The Odin Project & Youtube.
+        </span>
       </p>
       <a href="#" target="_blank" rel="noreferrer">
         <button className={about.btn}>
