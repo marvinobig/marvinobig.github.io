5f1e904c7445cab3b28cbea8b187c88b61f1bb9c
marvinobig <git config --global user.email>
2023-04-13 15:57:59 +0100

    display new projects & skill icons

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,12 +1,49 @@
 import pcMasterRace from "../assets/images/pc-masterrace.jpg";
+import cv_builder from "../assets/images/cv-builder.jpg";
 import elements from "../assets/images/elements.jpg";
+import track from "../assets/images/track.jpg";
 import css from "../assets/Dev-icons/front-end/css.png";
+import react from "../assets/Dev-icons/front-end/react.png";
 import js from "../assets/Dev-icons/front-end/javascript.png";
+import python from "../assets/Dev-icons/back-end/python.png";
 import html from "../assets/Dev-icons/front-end/html.png";
 import php from "../assets/Dev-icons/back-end/php.png";
 import mySql from "../assets/Dev-icons/back-end/mySql.png";
+import flask from "../assets/Dev-icons/back-end/flask.png";
+import sql from "../assets/Dev-icons/back-end/sql.png";
 
 const projects = [
+  {
+    name: "Bod Track",
+    img: track,
+    description: `A website to track your body weight across time so you can keep track of your progress. 
+    You can create/login to your own account, add and track your weight across time and have that data of 
+    your weight lost and gained over time visualised.`,
+    github: "https://github.com/marvinobig/bod-trac.git",
+    live: "https://mobig.pythonanywhere.com/",
+    technologies: [
+      {
+        img: python,
+        alt: "python",
+      },
+      {
+        img: flask,
+        alt: "flask",
+      },
+      {
+        img: sql,
+        alt: "sql",
+      },
+      {
+        img: html,
+        alt: "html",
+      },
+      {
+        img: css,
+        alt: "css",
+      },
+    ],
+  },
   {
     name: "PC Master Race",
     img: pcMasterRace,
@@ -16,14 +53,6 @@ const projects = [
     github: "https://github.com/marvinobig/pcmasterrace.git",
     live: "https://pcmasterracemobig.000webhostapp.com",
     technologies: [
-      {
-        img: html,
-        alt: "html",
-      },
-      {
-        img: css,
-        alt: "css",
-      },
       {
         img: php,
         alt: "PHP",
@@ -35,6 +64,36 @@ const projects = [
       {
         img: mySql,
         alt: "MySQL",
+      },
+      {
+        img: html,
+        alt: "html",
+      },
+      {
+        img: css,
+        alt: "css",
+      },
+    ],
+  },
+  {
+    name: "CV Builder",
+    img: cv_builder,
+    description: `This application is a CV web app that allows users to build a 
+    CV/resume from the ground up.`,
+    github: "https://github.com/marvinobig/cv-builder.git",
+    live: "https://cv-builder-web.netlify.app/",
+    technologies: [
+      {
+        img: js,
+        alt: "javascript",
+      },
+      {
+        img: react,
+        alt: "react js",
+      },
+      {
+        img: css,
+        alt: "css",
       },
     ],
   },
@@ -48,16 +107,16 @@ const projects = [
     live: "https://elements-wa.netlify.app/",
     technologies: [
       {
+        img: js,
+        alt: "javascript",
+      },
+      {
         img: html,
         alt: "html",
       },
       {
         img: css,
         alt: "css",
-      },
-      {
-        img: js,
-        alt: "javascript",
       },
     ],
   },
--- src/data/skillData.js
+++ src/data/skillData.js
@@ -3,13 +3,16 @@ import css from "../assets/Dev-icons/front-end/css.png";
 import scss from "../assets/Dev-icons/front-end/scss.png";
 import bootstrap from "../assets/Dev-icons/front-end/bootstrap.png";
 import js from "../assets/Dev-icons/front-end/javascript.png";
+import python from "../assets/Dev-icons/back-end/python.png";
 import react from "../assets/Dev-icons/front-end/react.png";
 import drupal from "../assets/Dev-icons/tools/drupal.png";
 import node from "../assets/Dev-icons/back-end/nodeJs.png";
 import express from "../assets/Dev-icons/back-end/expressJs.png";
+import flask from "../assets/Dev-icons/back-end/flask.png";
 import php from "../assets/Dev-icons/back-end/php.png";
 import mySql from "../assets/Dev-icons/back-end/mySql.png";
 import mongoDb from "../assets/Dev-icons/back-end/mongoDb.png";
+import sql from "../assets/Dev-icons/back-end/sql.png";
 import postgre from "../assets/Dev-icons/back-end/postgreSql.png";
 import git from "../assets/Dev-icons/tools/git.png";
 
@@ -35,6 +38,10 @@ const skills = [
     alt: "Bootstrap 5",
   },
   {
+    img: python,
+    alt: "Python",
+  },
+  {
     img: js,
     alt: "JavaScript",
   },
@@ -55,12 +62,20 @@ const skills = [
     alt: "Express JS",
   },
   {
+    img: flask,
+    alt: "Flask",
+  },
+  {
     img: postgre,
     alt: "PostgreSQL",
   },
   {
     img: mySql,
     alt: "MySQL",
+  },
+  {
+    img: sql,
+    alt: "SQLite",
   },
   {
     img: mongoDb,
