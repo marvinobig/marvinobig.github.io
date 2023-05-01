d1916e488955bba3e4884f6a6658ec29a98f7880
marvinobig <git config --global user.email>
2023-05-01 10:20:41 +0100

    update skills

--- src/data/skillData.js
+++ src/data/skillData.js
@@ -3,16 +3,11 @@ import css from "../assets/Dev-icons/front-end/css.png";
 import scss from "../assets/Dev-icons/front-end/scss.png";
 import bootstrap from "../assets/Dev-icons/front-end/bootstrap.png";
 import js from "../assets/Dev-icons/front-end/javascript.png";
-import python from "../assets/Dev-icons/back-end/python.png";
 import react from "../assets/Dev-icons/front-end/react.png";
 import drupal from "../assets/Dev-icons/tools/drupal.png";
 import node from "../assets/Dev-icons/back-end/nodeJs.png";
 import express from "../assets/Dev-icons/back-end/expressJs.png";
-import flask from "../assets/Dev-icons/back-end/flask.png";
-import php from "../assets/Dev-icons/back-end/php.png";
-import mySql from "../assets/Dev-icons/back-end/mySql.png";
 import mongoDb from "../assets/Dev-icons/back-end/mongoDb.png";
-import sql from "../assets/Dev-icons/back-end/sql.png";
 import postgre from "../assets/Dev-icons/back-end/postgreSql.png";
 import git from "../assets/Dev-icons/tools/git.png";
 
@@ -38,16 +33,8 @@ const skills = [
     alt: "Bootstrap 5",
   },
   {
-    img: python,
-    alt: "Python",
-  },
-  {
     img: js,
     alt: "JavaScript",
-  },
-  {
-    img: php,
-    alt: "PHP",
   },
   {
     img: react,
@@ -62,20 +49,8 @@ const skills = [
     alt: "Express JS",
   },
   {
-    img: flask,
-    alt: "Flask",
-  },
-  {
     img: postgre,
     alt: "PostgreSQL",
-  },
-  {
-    img: mySql,
-    alt: "MySQL",
-  },
-  {
-    img: sql,
-    alt: "SQLite",
   },
   {
     img: mongoDb,
