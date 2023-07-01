458faeac6d32ba8d4b5ce42f285cc3887d5712e3
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-07-01 21:52:23 +0100

    rename file

---
+++ astro-site/src/scripts/skillsData.js
@@ -0,0 +1,30 @@
+import html from "../assets/icons/front-end/html.png";
+import scss from "../assets/icons/front-end/scss.png";
+import js from "../assets/icons/front-end/javascript.png";
+import react from "../assets/icons/front-end/react.png";
+import postgre from "../assets/icons/back-end/postgreSql.png";
+
+const skillsData = [
+  {
+    img: html,
+    alt: "HTML",
+  },
+  {
+    img: scss,
+    alt: "SCSS",
+  },
+  {
+    img: js,
+    alt: "JavaScript",
+  },
+  {
+    img: react,
+    alt: "React JS",
+  },
+  {
+    img: postgre,
+    alt: "PostgreSQL",
+  },
+];
+
+export default skillsData;
