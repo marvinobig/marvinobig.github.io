e443842a5bc796f03bc3457ff1f83eca422a18e0
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-07-05 11:01:50 +0100

    style form button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,17 @@
 const Button = ({ btnType, eventFunction, btnText }) => {
+    const buttonStyle = {
+        height: "100%",
+        minHeight: "80px",
+        maxHeight: "80px",
+        backgroundColor: "#272727",
+        color: "#fcfcfc",
+        padding: "10px 20px",
+        border: "1px solid #272727"
+    }
+
     return (
         <button
+            style={buttonStyle}
             type={btnType}
             onClick={eventFunction}
         >
