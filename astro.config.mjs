81396f705074d17c9d6bec3b0dd344d58d5add80
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-07-06 10:41:58 +0100

    setup site for use with google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marvinobig.github.io/',
-  integrations: [react()]
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }), 
+    react(),
+  ],
 });
