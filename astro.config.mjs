05a956533a9068ab7fa3219d92e5c49000e37d47
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-07-06 10:51:04 +0100

    remove part town dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
-import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marvinobig.github.io/',
   integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }), 
     react(),
   ],
 });
