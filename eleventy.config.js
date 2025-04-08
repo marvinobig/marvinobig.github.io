f2b8f776e365254b383340aa66545cc9460a9b3e
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-08 20:31:53 +0200

    copy all assets

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,6 @@
 export default function (eleventyConfig) {
     // Set custom directories for output, includes, and data
-    eleventyConfig.addPassthroughCopy("assets/images");
-    eleventyConfig.addPassthroughCopy("assets/styles/css");
+    eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("site.webmanifest");
     eleventyConfig.addPassthroughCopy("CNAME");
     eleventyConfig.addPassthroughCopy("robots.txt");
