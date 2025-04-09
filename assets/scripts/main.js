0b02329109484afc65d3661ccd02f8cb59526d2d
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-09 15:08:30 +0200

    expand dropdown functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,3 +4,9 @@ const blogDropdown = document.querySelector("#blog-dropdown");
 blogDropdownBtn.addEventListener("click", () => {
     blogDropdown.classList.toggle("open");
 });
+
+document.addEventListener("click", (e) => {
+    if(!e.target.matches("#blog-dropdown") && !e.target.matches("#blog-dropdown-btn")) {
+        blogDropdown.classList.remove("open");
+    }
+});
