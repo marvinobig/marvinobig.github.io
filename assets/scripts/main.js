5ffd26418704744563223f0209d0e9338d217f5d
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-09 14:31:35 +0200

    add dropdown functionality

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+const blogDropdownBtn = document.querySelector("#blog-dropdown-btn");
+const blogDropdown = document.querySelector("#blog-dropdown");
+
+blogDropdownBtn.addEventListener("click", () => {
+    blogDropdown.classList.toggle("open")
+});
